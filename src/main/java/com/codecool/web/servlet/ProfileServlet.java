b587212cf2ae9de25c42305bf5499eeae8a33927
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-05-14 05:48:41 +0200

    profile servlet for the own coupons

--- src/main/java/com/codecool/web/servlet/ProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileServlet.java
@@ -1,6 +1,17 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.CouponDao;
+import com.codecool.web.dao.ShopDao;
+import com.codecool.web.dao.database.DatabaseCouponDao;
+import com.codecool.web.dao.database.DatabaseShopDao;
+import com.codecool.web.model.Coupon;
+import com.codecool.web.model.Shop;
 import com.codecool.web.model.User;
+import com.codecool.web.service.CouponService;
+import com.codecool.web.service.ShopService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleCouponService;
+import com.codecool.web.service.simple.SimpleShopService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,14 +19,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/protected/profile")
-public final class ProfileServlet extends HttpServlet {
+public final class ProfileServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) req.getSession().getAttribute("user");
-        req.setAttribute("user", user);
+        try (Connection connection = getConnection(req.getServletContext())) {
+            CouponDao couponDao = new DatabaseCouponDao(connection);
+            ShopDao shopDao = new DatabaseShopDao(connection);
+//            ShopService shopService = new SimpleShopService(shopDao);
+            CouponService couponService = new SimpleCouponService(couponDao, shopDao);
+
+            User user = (User) req.getSession().getAttribute("user");
+            List<Coupon> couponList = couponService.getCoupons(user.getId());
+        }
+        catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
         req.getRequestDispatcher("profile.jsp").forward(req, resp);
     }
 }
